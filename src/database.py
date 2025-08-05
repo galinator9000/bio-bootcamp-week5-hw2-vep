e9941d86654e121ca1415d084b06491ac564580c
Gali <gali.nil.un@gmail.com>
2025-08-05 13:10:59 +0000

    Refactor database setup and update data import logic for VEP homework

--- src/database.py
+++ src/database.py
@@ -4,12 +4,52 @@ from sqlalchemy.sql import func
 import os
 
 # Database connection
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/vep_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/vep_homework")
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
-# TODO: Database table definitions goes here
+class Gene(Base):
+    __tablename__ = 'gene'
+    gene_id = Column(Integer, primary_key=True)
+
+    symbol = Column(String, unique=True, nullable=False)
+    biotype = Column(String)
+
+    # Define a one-to-many relationship with variants
+    variants = relationship("Variant", back_populates="gene")
+
+class Variant(Base):
+    __tablename__ = 'variant'
+    variant_id = Column(Integer, primary_key=True)
+    
+    chrom = Column(String, nullable=False)
+    pos = Column(Integer, nullable=False)
+    ref = Column(String)
+    alt = Column(String)
+    rs_id = Column(String, nullable=True)
+    impact = Column(String)
+
+    # Gene relation
+    gene_id = Column(Integer, ForeignKey('gene.gene_id'))
+    gene = relationship("Gene", back_populates="variants")
+
+    # One-to-many relationship with variant calls
+    calls = relationship("VariantCall", back_populates="variant")
+
+class VariantCall(Base):
+    __tablename__ = 'variant_call'
+    vc_id = Column(Integer, primary_key=True)
+
+    # The INFO fields from the VCF file can be complex,
+    # so we'll store a few key ones.
+    sample_name = Column(String)
+    genotype = Column(String)
+    depth = Column(String)
+
+    # Relation to the Variant table
+    variant_id = Column(Integer, ForeignKey('variant.variant_id'))
+    variant = relationship("Variant", back_populates="calls")
 
 def create_tables():
     print("Creating database tables...")
--- src/import_vep_data.py
+++ src/import_vep_data.py
@@ -1,24 +1,84 @@
 import os
-from sqlalchemy.exc import IntegrityError
-from src.database import create_tables, Session
+import json
+from functools import cmp_to_key
+#from sqlalchemy.exc import IntegrityError
+#from src.database import create_tables, Session
 
-def parse_and_insert_vep_data():
-    print("Starting ClinVar data import...")
-    session = Session()
+def iterative_file_reader(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                yield line
+    except FileNotFoundError:
+        print(f"Error: The file at '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def transcript_selection(a, b):
+    def calculate_score(x):
+        return sum([
+            16 if x["canonical"] == "1" else 0,
+            8 if x["impact"] == "HIGH" else 0,
+            4 if x["impact"] == "MODERATE" else 0,
+            2 if x["impact"] == "LOW" else 0,
+            1 if x["impact"] == "MODIFIER" else 0,
+        ])
+
+    return calculate_score(b)- calculate_score(a)
+
+def parse_and_insert_vep_data(vep_data_folder_path="data"):
+    print("Starting VEP data import...")
+    #session = Session()
 
     try:
-        # TODO: Implement logic
-        pass
-    
+        # Path to the downloaded JSON file(s)
+        if not os.path.exists(vep_data_folder_path):
+            print(f"Error: Data folder not found at {vep_data_folder_path}")
+            return
+        
+        vep_data_files = [f for f in os.listdir(vep_data_folder_path) if f.endswith(".json")]
+        for vep_data_file in vep_data_files:
+            data_path = os.path.join(vep_data_folder_path, vep_data_file)
+
+            sample_name = data_path.replace(".json", "")
+
+            line_count = 0
+            for variant_raw_line in iterative_file_reader(data_path):
+                if line_count >= 1:
+                    break
+
+                line_count += 1
+                variant = json.loads(variant_raw_line)
+                print(variant)
+
+                # Parse VCF input part
+                vinp = variant["input"].split("\t")
+                vcf_inp = dict(zip(
+                    vinp[8].split(":"),
+                    [v if v != "." else None for v in vinp[9].split(":")]
+                ))
+
+                chrom = vinp[0]
+                pos = vinp[1]
+                ref = vinp[3]
+                alt = vinp[4]
+
+                # Transcript consequence selection
+                vtrxs = variant["transcript_consequences"]
+                worst_trx = sorted(vtrxs, key=cmp_to_key(transcript_selection))[0]
+
+                ### INSERTION IMPLEMENTATION GOES HERE
+
+        
     except Exception as e:
-        session.rollback()
+        #session.rollback()
         print(f"An error occurred: {e}")
     finally:
         # Final commit and close the session
-        session.commit()
-        session.close()
+        #session.commit()
+        #session.close()
         print("Data import finished.")
 
 if __name__ == "__main__":
-    create_tables()
-    parse_and_insert_vep_data()
+    # create_tables()
+    parse_and_insert_vep_data("data")
